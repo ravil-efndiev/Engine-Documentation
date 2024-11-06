808b9ee7a815aa6502d5a2280b0bd28c4e18bb33
Belokan <efendievravil@gmail.com>
2024-11-06 14:17:26 +0100

    card adaptivity fix

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,10 +13,10 @@ function Card({ title, text, image }: Props) {
       className={`
       flex flex-col items-center mx-auto
       bg-indigo-300 p-4 rounded-lg border border-indigo-500 
-      shadow-gray-800 shadow-xl w-96
+      shadow-gray-800 shadow-xl w-72 lg:w-96 md:w-80 sm:w-72
       hover:scale-[1.03] transition-transform`}
     >
-      <img src={image.src} alt={image.alt} className="max-w-80 rounded" />
+      <img src={image.src} alt={image.alt} className="max-w-[90%] rounded" />
       <h3 className="mt-6 mb-2">{title}</h3>
       <p className="text-lg text-center max-w-[80%]">{text}</p>
     </div>
